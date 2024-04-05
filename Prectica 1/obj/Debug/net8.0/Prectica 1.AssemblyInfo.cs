973cef4729852bf0710d58cb6a5a918538fdd9b1
maky99 <makyade99@gmail.com>
2024-04-05 11:15:37 -0300

    inmueble
    Arreglo de titulos

--- Prectica 1/obj/Debug/net8.0/Prectica 1.AssemblyInfo.cs
+++ Prectica 1/obj/Debug/net8.0/Prectica 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prectica 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e91f94a76bd11f9e62912d2d42555db586815b2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+764e58cc1d9dd2a80afb0e4833722dd07d5c02b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prectica 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prectica 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

94e83bf76fd52aa95dfc19b20b92341c5172c7bb
Daniel Alfonzo <makyade99@gmail.com>
2024-04-06 11:23:26 -0300

    trabajando en contrato
    viendo los campos

--- Prectica 1/Models/Contrato.cs
+++ Prectica 1/Models/Contrato.cs
@@ -4,11 +4,13 @@ namespace Prectica_1.Models
     {
         public int id_contrato { get; set; }
         public DateTime desde { get; set; }
+        public int meses { get; set; }
         public DateTime hasta { get; set; }
         public string? detalle { get; set; }
         public DateTime finalizacion_anticipada { get; set; }
         public double monto { get; set; }
         public double multa { get; set; }
+        public int estado { get; set; }
         public int id_inquilino { get; set; }
     }
 }
--- Prectica 1/obj/Debug/net8.0/Prectica 1.AssemblyInfo.cs
+++ Prectica 1/obj/Debug/net8.0/Prectica 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prectica 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2105f14a607bae3550a4824296a7987f4927da90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6635fa92115659dd9f2e16b023b877d93a907530")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prectica 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prectica 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

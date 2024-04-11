8fa9ea7998440650befcc36318d893ad03659e96
Daniel Alfonzo <makyade99@gmail.com>
2024-04-11 14:15:22 -0300

    vistas
    s

--- Prectica 1/obj/Debug/net8.0/Prectica 1.AssemblyInfo.cs
+++ Prectica 1/obj/Debug/net8.0/Prectica 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prectica 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72abdf800c7921493e222c8ca59ceced76ac61d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6bc24d0c9e5c649ad6e73e48e17ab4f0b7ab6f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prectica 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prectica 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

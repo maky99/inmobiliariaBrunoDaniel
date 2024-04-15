ca21e106457425044be45e5c067e6a7d0b30da63
maky99 <makyade99@gmail.com>
2024-04-15 14:00:56 -0300

    imegenes

--- Prectica 1/obj/Debug/net8.0/Prectica 1.AssemblyInfo.cs
+++ Prectica 1/obj/Debug/net8.0/Prectica 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prectica 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3124d125838732306e00ab24742090bb8977d16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbeee8c01ce7ff8d00a20622a20bf728d9afd92f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prectica 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prectica 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
